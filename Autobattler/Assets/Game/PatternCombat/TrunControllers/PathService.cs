7ed3df5f5b21f169503987c27278b65795ca5bdd
Ar0cka <vanoshul@gmail.com>
2026-05-11 23:35:36 +1200

    feat: Update base turn controller and add Manual Turn Controller

---
+++ Autobattler/Assets/Game/PatternCombat/TrunControllers/PathService.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using DefaultNamespace.Pathfiender;
+using Game.Combat.Grid;
+using Grid;
+using UnityEngine;
+
+namespace Game.PatternCombat.TrunControllers
+{
+    public class PathService : IPathService
+    {
+        private Bfs _pathfinder;
+        private ITurnGrid _turnGrid;
+
+        public PathService(Bfs pathfinder, ITurnGrid turnGrid)
+        {
+            _pathfinder = pathfinder;
+            _turnGrid = turnGrid;
+        }
+
+        public List<GridData> FindPath(int unitX, int unitY, int targetX, int targetY) => 
+            _pathfinder.CalculatePath(unitX, unitY, targetX, targetY);
+    }
+
+    public interface IPathService
+    {
+        public List<GridData> FindPath(int unitX, int unitY, int targetX, int targetY);
+    }
+}
